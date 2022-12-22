7364e070be9009a2723f6e13c5915608ba331e1f
Scala Steward <scala_steward@virtuslab.com>
2022-12-22 16:03:01 +0000

    Update semanticdb-scalac to 4.7.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
     val kindProjector    = "0.13.2"
     val logback          = "1.4.5"
     val organizeImports  = "0.6.0"
-    val semanticDB       = "4.6.0"
+    val semanticDB       = "4.7.1"
 
     val weaver = "0.8.1"
   }
