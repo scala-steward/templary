080db5053508a17bc32b7ec8f7e4dd340983aace
Scala Steward <scala_steward@virtuslab.com>
2023-04-21 16:35:20 +0000

    Update logback-classic to 1.4.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
 
     val betterMonadicFor = "0.3.1"
     val kindProjector    = "0.13.2"
-    val logback          = "1.4.6"
+    val logback          = "1.4.7"
     val organizeImports  = "0.6.0"
     val semanticDB       = "4.7.6"
 
